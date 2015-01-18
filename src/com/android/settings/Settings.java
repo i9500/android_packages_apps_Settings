1b4356a10252d7c1efc9a098ab57f779748d5964
snak3ater <saeedyasir91@gmail.com>
2015-01-18 04:43:55 +0000

    Rotation tile: Longclick action [2/2]

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -108,5 +108,6 @@ public class Settings extends SettingsActivity {
     public static class LiveDisplayActivity extends SettingsActivity { /* empty */ }
     public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
     public static class QSTilesSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class DisplayRotationSettingsActivity extends SettingsActivity { /* empty */ }
 }
 
--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -78,6 +78,7 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.cyanogenmod.qs.QSTiles;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
@@ -328,7 +329,8 @@ public class SettingsActivity extends Activity
             LockScreenSettings.class.getName(),
             LiveDisplay.class.getName(),
             HeadsUpSettings.class.getName(),
-            QSTiles.class.getName()
+            QSTiles.class.getName(),
+            DisplayRotation.class.getName()
     };
 
 
