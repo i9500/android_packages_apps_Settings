b1edcddfa11ba692235a93c29fce6c0144b19082
Michael Bestas <mikeioannina@gmail.com>
2015-03-07 05:22:23 +0200

    Allow launching display rotation & lock screen settings externally
    Change-Id: I69168fea2a43cfa7f2b8474042c35720f4431040

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -79,6 +79,7 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.cyanogenmod.qs.QSTiles;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
@@ -334,7 +335,8 @@ public class SettingsActivity extends Activity
             HeadsUpSettings.class.getName(),
             QSTiles.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
-            AmbientSettings.class.getName()
+            AmbientSettings.class.getName(),
+            DisplayRotation.class.getName()
     };
 
 
