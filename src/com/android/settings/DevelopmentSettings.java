ca1718868f4e667d3381bfce3833527ee874115c
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-15 20:57:27 +0000

    Revert "Settings: make unaccounted and over-counted battery usage configurable"
    This reverts commit https://github.com/Euphoria-OS/android_packages_apps_Settings/commit/e5e2a63e244cb3304df065338d340dc888a3bc59

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -98,11 +98,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
      */
     public static final String PREF_SHOW = "show";
 
-    /**
-     * Whether to show unnacounted and over-counted battery stats.  Default is false.
-     */
-    public static final String SHOW_UNAC_AND_OVERCOUNTED_STATS = "show_unac_and_overcounted_stats";
-
     private static final String ENABLE_ADB = "enable_adb";
     private static final String ADB_NOTIFY = "adb_notify";
     private static final String ADB_TCPIP = "adb_over_network";
@@ -209,7 +204,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private boolean mHaveDebugSettings;
     private boolean mDontPokeProperties;
 
-    private SwitchPreference mShowUnacAndOvercounted;
     private SwitchPreference mEnableAdb;
     private SwitchPreference mAdbNotify;
     private SwitchPreference mAdbOverNetwork;
@@ -316,8 +310,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.development_prefs);
 
         mAdvancedSettings = (SecureSettingSwitchPreference) findPreference(KEY_ADVANCED_MODE);
-
-        mShowUnacAndOvercounted = findAndInitSwitchPref(SHOW_UNAC_AND_OVERCOUNTED_STATS);
 
         final PreferenceGroup debugDebuggingCategory = (PreferenceGroup)
                 findPreference(DEBUG_DEBUGGING_CATEGORY_KEY);
@@ -743,7 +735,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         updateAllOptions();
         mDontPokeProperties = false;
         pokeSystemProperties();
-        mShowUnacAndOvercounted.setChecked(false);
     }
 
     private void updateRootAccessOptions() {
